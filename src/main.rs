2c655307619744c1eb435d0b230395046d1d7e1e
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-28 00:55:57 +0530

    add: added example for string slicing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..]; // example of creating a slice of the string
+    println!("{}", string_slice);
+
+    // let server = Server::new("127.0.0.1:8080".to_string());
+    // server.run();
 }
 
 struct Server {
