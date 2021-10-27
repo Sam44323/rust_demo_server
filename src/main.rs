8aad87cb1e7f28b9eb59c59e02711b02ec0628f7
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-28 00:56:47 +0530

    update: updated the string getter using String::from

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..]; // example of creating a slice of the string
-    println!("{}", string_slice);
-
-    // let server = Server::new("127.0.0.1:8080".to_string());
-    // server.run();
+    let server = Server::new(String::from("127.0.0.1:8080"));
+    server.run();
 }
 
 struct Server {
