91238c217d50fd7cdc2c80f36511e581c13526db
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-29 00:02:47 +0530

    add: added module files for server

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
 use http::request::Request;
 use server::Server;
 
+mod server;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-mod server {
-
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Server { addr }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
-        }
-    }
 }
 
 mod http {
