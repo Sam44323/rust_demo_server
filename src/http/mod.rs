ebf63dd0217284634e519fdef34242e8718ca66a
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-29 00:29:12 +0530

    add: added docs for mod

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,7 @@
+// exposing the module from single point mod by convention
 pub mod method;
 pub mod request;
 
-// example of exposing the module from the file
+// example of exposing the module specifics from the file
 pub use method::Method;
 pub use request::Request;
