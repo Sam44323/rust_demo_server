b7fd0f7da4db785228c3ed6941c7bb038f2d8527
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-05 22:19:36 +0530

    add: added FromStr trait for Method enum for converting

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,6 @@
+use super::request::ParseError;
+use std::str::FromStr;
+
 pub enum Method {
   GET,
   POST,
@@ -8,4 +11,23 @@ pub enum Method {
   OPTIONS,
   TRACE,
   PATCH,
+}
+
+impl FromStr for Method {
+  type Err = ParseError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "GET" => Ok(Method::GET),
+      "POST" => Ok(Method::POST),
+      "PUT" => Ok(Method::PUT),
+      "DELETE" => Ok(Method::DELETE),
+      "HEAD" => Ok(Method::HEAD),
+      "CONNECT" => Ok(Method::CONNECT),
+      "OPTIONS" => Ok(Method::OPTIONS),
+      "TRACE" => Ok(Method::TRACE),
+      "PATCH" => Ok(Method::PATCH),
+      _ => Err(ParseError::InvalidMethod),
+    }
+  }
 }
