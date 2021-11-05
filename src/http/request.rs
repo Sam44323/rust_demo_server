6dd4501752b8df34b008a1dee47f7d306666548c
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-05 16:05:15 +0530

    add: added implementations for the debug and the display trait for the parse error enum

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method; // using the super as request belongs to the same folder as requests
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult}; // using a special Result type used for Display Trait
 
 pub struct Request {
   path: String,
@@ -25,3 +27,30 @@ pub enum ParseError {
   InvalidProtocol, // if the HTTP protocol is not 1.1
   InvalidMethod,   // if the method is not a valid HTTP method
 }
+
+impl ParseError {
+  fn message(&self) -> &str {
+    match self {
+      Self::InvalidRequest => "Invalid Request",
+      Self::InvalidEncoding => "Invalid Encoding",
+      Self::InvalidProtocol => "Invalid Protocol",
+      Self::InvalidMethod => "Invalid Method",
+    }
+  }
+}
+
+// implementing the Display trait for the ParseError enum
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+    // writing the string representation of the enum(message) to the formatter
+    write!(f, "{}", self.message())
+  }
+}
+
+// implementing the Debug trait for the ParseError enum
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+impl Error for ParseError {}
